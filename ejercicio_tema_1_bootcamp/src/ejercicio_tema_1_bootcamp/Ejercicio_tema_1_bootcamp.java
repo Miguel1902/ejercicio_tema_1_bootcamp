87347afce47684e41c0d28e79509fc06d1eaebdf
Miguel1902 <74044262+Miguel1902@users.noreply.github.com>
2022-09-01 16:53:00 +0200

    Add files via upload

---
+++ ejercicio_tema_1_bootcamp/src/ejercicio_tema_1_bootcamp/Ejercicio_tema_1_bootcamp.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_tema_1_bootcamp;
+
+/**
+ *
+ * @author migue
+ */
+public class Ejercicio_tema_1_bootcamp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 0;
+        
+        long o = 78493020L ;
+        
+        double doble = 1.12;
+        
+        boolean si = true;
+        
+        String hola = "que masas crack";
+        
+        
+        System.out.println("el int es: "+i);
+        System.out.println("el long es: "+o);
+        System.out.println("el double es: "+doble);
+        System.out.println("el boolean es: "+si);
+        System.out.println("el String es: "+hola);
+    }
+    
+}
